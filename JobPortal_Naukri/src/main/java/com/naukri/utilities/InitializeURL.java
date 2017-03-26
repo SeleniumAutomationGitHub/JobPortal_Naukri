f02fae6f32e725f36471e9d11865da2aa8fd4564
SeleniumAutomationGitHub <seleniumautomaton@gmail.com>
2017-03-26 12:55:50 +0530

    new method removed

--- JobPortal_Naukri/src/main/java/com/naukri/utilities/InitializeURL.java
+++ JobPortal_Naukri/src/main/java/com/naukri/utilities/InitializeURL.java
@@ -9,11 +9,4 @@ public class InitializeURL {
 			InitializeDriver.driver.get(url);
 		}
 	}
-	
-	public void lunchURL(String url, int abc){
-		if (url!= null){
-			InitializeDriver.driver.manage().window().maximize();
-			InitializeDriver.driver.get(url);
-		}
-	}
 }
